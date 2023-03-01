fc92719f4a99c84ab1ba3a611b13f7084675bef2
Agrima <er.agrima.malhotra@gmail.com>
2023-03-01 15:35:01 +0530

    feat: add routing to Home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { Header } from './components/index.js';
+import { HOME_ROUTE } from './constants/routes.js';
+import { Home } from './pages/index.js';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Routes>
+        <Route path={HOME_ROUTE} element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,3 @@
 export { default as Header } from './Header/index.jsx';
+export { default as EventCard } from './EventCard/index.jsx';
+export { default as EventCardGrid } from './EventCardGrid/index.jsx';
