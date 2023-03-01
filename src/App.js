b1bb53e844647514c33cef0cce62435a633431c7
Agrima <er.agrima.malhotra@gmail.com>
2023-03-01 12:06:15 +0530

    feat: add Header component to all pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Header } from './components/index.js';
+import './App.css';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
