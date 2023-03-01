fd883b483341df595a2650cf76f764b891226f9a
Agrima <er.agrima.malhotra@gmail.com>
2023-03-01 12:03:44 +0530

    feat: add Header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+  return (
+    <div className="header padding">
+      <h1>EVENTIFY</h1>
+    </div>
+  );
+};
+
+export default Header;
