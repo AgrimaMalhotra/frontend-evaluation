454b83e9d4050b56da3deca8cdc3b78e26728510
Agrima <er.agrima.malhotra@gmail.com>
2023-03-01 17:05:54 +0530

    opt: refactored code

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,28 +1,32 @@
-/*eslint-disable*/
 import React, { useEffect, useState } from 'react';
-import { EventCardGrid } from '../../components/index.js';
+import {
+  EventCardGrid,
+  Header,
+} from '/Users/agrima_malhotra/Desktop/frontendevaluation/src/components/index.js';
 import makeRequest from '../../utils/makeRequest/makeRequest.js';
 import { GET_ALL_EVENTS_DATA } from '../../constants/apiEndPoints.js';
 
-const Home = () =>{
+const Home = () => {
   const [allEventsData, setAllEventsData] = useState([]);
-useEffect(()=>{
-   makeRequest({ ...GET_ALL_EVENTS_DATA }, {}).then(
-    (data)=>{
-setAllEventsData(data);
-    }
-   );
-},[]);
-  return allEventsData?(
-    <div className="home-page">
-      <EventCardGrid allEventsData={allEventsData} />
-    </div>
-  ):
-  (
-    <div className='page-loader'>
-      <p>Loading...</p>
-    </div>
+  useEffect(() => {
+    makeRequest({ ...GET_ALL_EVENTS_DATA }, {}).then((data) => {
+      setAllEventsData(data);
+    });
+  }, []);
+  return (
+    <>
+      <Header />
+      {allEventsData ? (
+        <div className="home-page">
+          <EventCardGrid allEventsData={allEventsData} />
+        </div>
+      ) : (
+        <div className="page-loader">
+          <p>Loading...</p>
+        </div>
+      )}
+    </>
   );
-}
+};
 
 export default Home;
